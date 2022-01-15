48fb8d974007688bd92425f36b54779736c86e8e
ElegantDumbass <alfadurros@gmail.com>
2022-01-15 14:51:46 +0700

    pandu

---
+++ resources/views/pandu.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>SMK</title>
+    <link rel="stylesheet" href="{{asset('smk/css/smk.css')}}">
+</head>
+
+<body>
+    <div class="letter-open">
+        <h1 class="title">Aplikasi Dalam Tahap Pengembangan</h1>
+        <div class="messagebox">
+            Nama Project : SMK <br>
+            <br>
+            Pengembang : Pandu Yassar Alfarros<br>
+            <br>
+            Waktu Prod : Januari - Maret 2022
+            <br>
+            <br>
+            <div align="center">
+                <button class="mybtn" onclick="masuk()">MASUK</button>
+            </div>
+        </div>
+    </div>
+    <script src="smk/js/smk.js"></script>
+</body>
+
+</html>
